bbdbfb8071df6c2259b7468958174e7f74318369
namanjaidka02 <namanjaidka89@gmail.com>
2022-08-03 12:57:39 +0530

    new method

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
+let fnum;
+let snum;
 
 function add(){
-     let fnum = document.getElementById("number1").value;
-     fnum = Number(fnum)
-     let snum = document.getElementById("number2").value;
-     snum = Number(snum);
+    fnum = document.getElementById("number1").value;
+    fnum = Number(fnum)
+    snum = document.getElementById("number2").value;
+    snum = Number(snum)
      let total = fnum + snum;
      document.getElementById("result").innerHTML = total;
 }
 function subtract(){
-    let fnum = document.getElementById("number1").value;
-     fnum = Number(fnum)
-     let snum = document.getElementById("number2").value;
-     snum = Number(snum);
+    
      let total = fnum - snum;
      document.getElementById("result").innerHTML = total;
 }
 function multiply(){
-    let fnum = document.getElementById("number1").value;
-     fnum = Number(fnum)
-     let snum = document.getElementById("number2").value;
-     snum = Number(snum);
+   
      let total = fnum * snum;
      document.getElementById("result").innerHTML = total;
 }
+
 function arithmetic(){
-     add();
+    
+     add()
      subtract();
      multiply();
 }
